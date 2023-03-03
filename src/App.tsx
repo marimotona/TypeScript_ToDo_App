b8b377fcb969d79edff33e8eb6e936cec1a9fff8
marimotona <ssmmo.06021224@gmail.com>
2023-03-04 00:06:45 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>TypeScript ToDo</header>
+      <form>
+        <input type="text" onChange={() => {}} className="inputText" />
+        <input type="submit" value="Button" className="changeButton" />
+      </form>
     </div>
   );
 }
